cb9cec0899b41e4699f0ab5ba98491b4a174e7f9
linhbh25796 <linhbh25796@gmail.com>
2019-11-06 15:45:37 +0700

    commit

---
+++ src/app/color/color.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.css']
+})
+export class ColorComponent implements OnInit {
+  background = '#00e011';
+  onChangeColor(value) {
+    this.background = value;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
